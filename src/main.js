9ec97d186e877cb3cb10d3eb9c7a2526ea9739c0
Andrés Osante <andres.osante@gorillalogic.com>
2021-03-10 00:25:33 -0600

    add vuetify

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue';
-import App from './App.vue';
+import App from './app/app.vue';
 import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
+import '@mdi/font/css/materialdesignicons.css';
+import 'roboto-fontface/css/roboto/roboto-fontface.css';
 
 Vue.config.productionTip = false;
 
@@ -10,5 +12,5 @@ new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App),
+  render: (h) => h(App),
 }).$mount('#app');
