6aa7c44e85f2280b107be15af0b82af15b1e9470
Andrés Osante <andres.osante@gorillalogic.com>
2021-03-13 21:25:03 -0600

    add player component

--- src/components/main-layout/main-layout.js
+++ src/components/main-layout/main-layout.js
@@ -1,10 +1,12 @@
 import AppBar from '../app-bar/app-bar.vue';
 import MenuDrawer from '../app-menu-drawer/app-menu-drawer.vue';
+import Player from '../player/player.vue';
 
 export default {
   name: 'MainLayout',
   components: {
     AppBar,
     MenuDrawer,
+    Player,
   },
 };
