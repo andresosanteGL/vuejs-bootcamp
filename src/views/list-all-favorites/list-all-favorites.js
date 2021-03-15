2e4089ffbedecbc8bd565184d3577dbb03c1edeb
Andrés Osante <andres.osante@gorillalogic.com>
2021-03-14 23:08:10 -0600

    connect vue components with vuex store

--- src/views/list-all-favorites/list-all-favorites.js
+++ src/views/list-all-favorites/list-all-favorites.js
@@ -1,6 +1,8 @@
 import { filterBySearchTerm } from '../../utils/filterBySearchTerm';
-import api from '../../api';
 import TrackList from '../../components/track-list/track-list.vue';
+
+import { addAndRemoveFavorite } from '../../store/modules/favorites/utils';
+import { FETCH_FAVORITES, ALL_FAVORITES_LIMIT } from '../../store/modules/favorites/types';
 
 const MAX_LIST_ITEMS = 100;
 
@@ -15,12 +17,18 @@ export default {
       searchTerm: '',
     };
   },
+  methods: {
+    ...addAndRemoveFavorite(),
+  },
   computed: {
+    favorites() {
+      return this.$store.getters[ALL_FAVORITES_LIMIT](MAX_LIST_ITEMS);
+    },
     favoriteList() {
       return filterBySearchTerm(this.searchTerm, this.favorites);
     },
   },
   async mounted() {
-    this.favorites = await api.getFavoritesTracks(MAX_LIST_ITEMS);
+    this.$store.dispatch(FETCH_FAVORITES);
   },
 };
